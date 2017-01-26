1b73fabcad981a3222659b42307c1e82731060b7
Rajat Bajpai <rajat.bajpai.77@gmail.com>
2017-01-26 23:14:27 +0530

    makeathon-2017

---
+++ testDB/aise/migrations/0004_auto_20170124_1048.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aise', '0003_auto_20170124_1040'),
+    ]
+
+    operations = [
+        migrations.AlterModelTable(
+            name='details',
+            table='details',
+        ),
+    ]
